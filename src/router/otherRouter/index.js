cdaa942781ff7172bc129e454c4fd311198076f7
李杨 <1711467488@qq.com>
2018-03-05 16:09:30 +0800

    no message

--- src/router/otherRouter/index.js
+++ src/router/otherRouter/index.js
@@ -8,10 +8,6 @@ import cj from './_manage/cj';
 import dj from './_manage/dj';
 import fh from './_manage/fh';
 
-import stage1 from './_pickOut/stage1.js';
-import stage2 from './_pickOut/stage2.js';
-import stage3 from './_pickOut/stage3.js';
-
 // 作为Main组件的子页面展示但是不在左侧菜单显示的路由写在otherRouter里
 export default {
     path: '/',
@@ -21,9 +17,6 @@ export default {
         ...core,
         ...sys,
         ...gen,
-        ...stage1,
-        ...stage2,
-        ...stage3,
         ...cj,
         ...dj,
         ...fh
