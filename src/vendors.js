38dc26c9d6180dfc684fc146d9f3c4f3938827c4
李杨 <1711467488@qq.com>
2018-03-05 16:37:05 +0800

    no message

--- src/vendors.js
+++ src/vendors.js
@@ -4,5 +4,4 @@ import VueRouter from 'vue-router';
 import { all, spread, axios, axiosToken, axiosErrorHandler } from '@/plugins/axios.js';
 import Vuex from 'vuex';
 import Cookies from 'js-cookie';
-import clipboard from 'clipboard';
 import moment from 'moment';
